98d282f7f34aa24e463e1cb2e5020ad19e3c07ea
Moojun <mgim32137@gmail.com>
2024-12-02 22:15:04 +0900

    initial Setup

---
+++ src/main/java/com/dailyhub/untitled_01_be/Untitled01BeApplication.java
@@ -0,0 +1,13 @@
+package com.dailyhub.untitled_01_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Untitled01BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Untitled01BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailyhub/untitiled_01_be/Untitled01BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyhub.untitiled_01_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Untitled01BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
